b4a20b12a95969a896524a29c1b290a50b3f194a
David Sheinbein <davesheinbein@gmail.com>
2020-10-14 14:31:13 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function convert() {
+function convertTextToBinary() {
 	var output = document.getElementById('inputB');
 	var input = document.getElementById('inputA').value;
 	output.value = '';
@@ -6,4 +6,14 @@ function convert() {
 		output.value +=
 			input[i].charCodeAt(0).toString(2) + ' ';
 	}
+}
+
+function convertBinaryToText() {
+	// var output = document.getElementById('inputB');
+	// var input = document.getElementById('inputA').value;
+	// output.value = '';
+	// for (i = 0; i < input.length; i++) {
+	// 	output.value +=
+	// 		input[i].charCodeAt(0).toString(2) + ' ';
+	// }
 }
