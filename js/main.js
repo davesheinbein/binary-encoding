a17ef07a082c7594b856a5808fe990c5a22f4b3b
David Sheinbein <davesheinbein@gmail.com>
2020-10-15 16:31:46 -0700

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function convertTextToBinary() {
 	var output = document.getElementById('inputB');
 	var input = document.getElementById('inputA').value;
 	output.value = '';
-	for (i = 0; i < input.length; i+=8) {
+	for (i = 0; i < input.length; i++) {
 		output.value +=
 			input[i].charCodeAt(0).toString(2) + ' ';
 	}
