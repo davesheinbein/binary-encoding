5d5e0f23394dad78f2d762bdd7d7bb7dc24a0f06
David Sheinbein <davesheinbein@gmail.com>
2020-10-15 16:29:31 -0700

    update

--- js/main.js
+++ js/main.js
@@ -2,19 +2,10 @@ function convertTextToBinary() {
 	var output = document.getElementById('inputB');
 	var input = document.getElementById('inputA').value;
 	output.value = '';
-	for (i = 0; i < input.length; i++) {
+	for (i = 0; i < input.length; i+=8) {
 		output.value +=
 			input[i].charCodeAt(0).toString(2) + ' ';
 	}
 }
 
-function convertBinaryToText() {
-	// var output = document.getElementById('inputB');
-	// var input = document.getElementById('inputA').value;
-	// output.value = '';
-	// for (i = 0; i < input.length; i++) {
-	// 	output.value +=
-	// 		input[i].charCodeAt(0).toString(2) + ' ';
-	// }
-	// 
-}
+function convertBinaryToText() {}
